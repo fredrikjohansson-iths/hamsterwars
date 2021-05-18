83c9e690e1260b473af6308d7554babcfb8937a2
Stig Fredrik Johansson <fredrik.johansson@iths.se>
2021-05-18 20:58:09 +0200

    ready for deployment

--- routes/hamsters.js
+++ routes/hamsters.js
@@ -1,23 +1,124 @@
-const getDb = require('../fbauth.js')
-const express = require('express')
+const getDb = require("../fbauth.js");
+const express = require("express");
+const { body, validationResult } = require("express-validator");
 
-const db = getDb()
-const router = express.Router()
+const db = getDb();
+const router = express.Router();
 
-
-
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
 	try {
-		const snapshot = await db.collection('hamsters').get();
-			snapshot.forEach((doc) => {
-				console.log(doc.id, '=>', doc.data());
-				res.send(doc.name)
-			});
-		
+		const snapshot = await db.collection("hamsters").get();
+
+		let docData = [];
+
+		snapshot.forEach((doc) => {
+			docData.push(doc.data());
+		});
+
+		res.status(200).send(docData);
 	} catch (err) {
-		res.status(500).send(err.message)
+		res.status(500).send(err.message);
 	}
+});
 
-})
+router.get("/random", async (req, res) => {
+	try {
+		const snapshot = await db.collection("hamsters").get();
 
-module.exports = router 
+		let docData = [];
+
+		snapshot.forEach((doc) => {
+			docData.push(doc.data());
+		});
+
+		let rng = Math.floor(Math.random() * docData.length);
+
+		res.status(200).send(docData[rng]);
+	} catch (err) {
+		res.status(500).send(err.message);
+	}
+});
+
+router.get("/:id", async (req, res) => {
+	try {
+		const id = req.params.id;
+		const docRef = await db.collection("hamsters").doc(id).get();
+
+		if (!docRef.exists) {
+			res.status(404);
+		}
+
+		res.status(200).send(docRef.data());
+	} catch (err) {
+		res.status(500).send(err.message);
+	}
+});
+
+router.post(
+	"/",
+	body("name").exists().notEmpty().isString(),
+	body("age").exists().notEmpty().isInt(),
+	body("favFood").exists().notEmpty().isString(),
+	body("loves").exists().notEmpty().isString(),
+	body("imgName").exists().notEmpty().isString(),
+	async (req, res) => {
+		try {
+			const errors = validationResult(req);
+			if (!errors.isEmpty()) {
+				return res.status(400).json({ errors: errors.array() });
+			}
+			const docRef = await db.collection("hamsters").add(req.body);
+			res.status(200).send({ id: docRef.id, data: req.body });
+		} catch (err) {
+			res.status(500).send(err.message);
+		}
+	}
+);
+
+router.put(
+	"/:id",
+	body("name").isString(),
+	body("age").isInt(),
+	body("favFood").isString(),
+	body("loves").isString(),
+	body("imgName").isString(),
+	async (req, res) => {
+		try {
+			const errors = validationResult(req);
+			if (!errors.isEmpty()) {
+				return res.status(400).json({ errors: errors.array() });
+			}
+			const id = req.params.id;
+			const docRef = await db.collection("hamsters").doc(id).get();
+
+			if (!docRef.exists) {
+				return res.status(404);
+			}
+			const docSet = await db
+				.collection("hamsters")
+				.doc(id)
+				.set(req.body, { merge: true });
+			res.status(200);
+		} catch (err) {
+			res.status(500).send(err.message);
+		}
+	}
+);
+
+router.delete("/:id", async (req, res) => {
+	try {
+		const id = req.params.id;
+		const docRef = await db.collection("hamsters").doc(id).get();
+
+		if (!docRef.exists) {
+			res.status(404);
+		}
+
+		const docDel = await db.collection("hamsters").doc(id).delete();
+		res.status(200);
+	} catch (err) {
+		res.status(500).send(err.message);
+	}
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require("express");
 const path = require('path')
 const cors = require("cors");
 const getDb = require("./fbauth.js")
-const localData = require("./data.json");
+
+
 const hamsters = require('./routes/hamsters.js')
+
+
 const staticFolder = path.join(__dirname, 'frontend')
 
 const app = express();
@@ -16,12 +19,6 @@ app.use(cors())
 app.use(express.static(staticFolder))
 
 app.use('/hamsters', hamsters)
-
-// app.use(async function (req, res, next) {
-//   for (const element of localData)
-//     await db.collection("hamsters").doc(`${element.id}`).set(element);
-//   next()
-// });
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
