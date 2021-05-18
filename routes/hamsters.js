3edfd01f6073e268f5b767b581a8c13952882bcb
Stig Fredrik Johansson <fredrik.johansson@iths.se>
2021-05-18 17:38:22 +0200

    error fix

--- routes/hamsters.js
+++ routes/hamsters.js
@@ -4,10 +4,11 @@ const express = require('express')
 const db = getDb()
 const router = express.Router()
 
-const snapshot = await db.collection('hamsters').get();
+
 
 router.get('/', async (req, res) => {
 	try {
+		const snapshot = await db.collection('hamsters').get();
 			snapshot.forEach((doc) => {
 				console.log(doc.id, '=>', doc.data());
 				res.send(doc.name)
